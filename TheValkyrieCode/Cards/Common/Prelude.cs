f96c19bef391ab4319a79468394fcce61a1b1f60
PrincessFelicie <taxouck1@live.fr>
2026-06-26 17:46:37 +0200

    fixed valkyrie sprite not flipping against kaiser crab boss fixed a warning in the log console related to the Byrd Nest relic added a placeholder animation for the character coming back to life in multiplayer New Ancient dialogue

--- TheValkyrieCode/Cards/Common/Prelude.cs
+++ TheValkyrieCode/Cards/Common/Prelude.cs
@@ -20,7 +20,7 @@ public class Prelude : TheValkyrieCard
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         await CommonActions.CardAttack(this, play.Target).Execute(choiceContext);
-        foreach (CardModel card2 in PileType.Draw.GetPile(Owner).Cards.Where<CardModel>((Func<CardModel, bool>) (c => c.IsUpgradable)).TakeRandom<CardModel>(DynamicVars.Cards.IntValue, Owner.RunState.Rng.CombatCardSelection))
+        foreach (CardModel card2 in PileType.Draw.GetPile(Owner).Cards.Where(c => c.IsUpgradable).TakeRandom(DynamicVars.Cards.IntValue, Owner.RunState.Rng.CombatCardSelection))
         {
             CardCmd.Upgrade(card2);
             CardCmd.Preview(card2);
